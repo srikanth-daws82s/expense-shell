12b6d464b423338bb99134a097dcb1606f89ad58
srikanth <bommisettysrikanth88@gmail.com>
2025-01-18 17:27:23 +0530

    expense shell

--- mysqldb.sh
+++ mysqldb.sh
@@ -17,10 +17,10 @@ VALIDATE(){
 
     if [ $1 -ne 0 ]
      then
-          echo "$2... $R failure $N"
+          echo -e "$2... $R Failure $N"
           exit 1
      else
-        echo "$2... $G success $N"
+        echo  -e "$2... $G Success $N"
 
     fi  
 
