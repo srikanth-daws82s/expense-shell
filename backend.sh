af5aab38f511fbe2187682a2578ce563633d1a60
srikanth <bommisettysrikanth88@gmail.com>
2025-01-18 20:30:38 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -48,20 +48,14 @@ VALIDATE $? "Enabling NodeJS 20"
 dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing NodeJS"
 
-id expense &>>$LOG_FILE_NAME
-if [ $? -ne 0 ]
-then
-    useradd expense &>>$LOG_FILE_NAME
-    VALIDATE $? "Adding expense user"
-else
-    echo -e "expense user already exists ... $Y SKIPPING $N"
-fi
+useradd expense
+VALIDATE $? "Adding expense user"
 
 mkdir /app &>>$LOG_FILE_NAME
-VALIDATE $? " Create app directory"
+VALIDATE $? "Create app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
-VALIDATE $? " Downloading backend"
+VALIDATE $? "Downloading backend"
 
 cd /app
 
