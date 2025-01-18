fe553b86aeb892b1a2f71f0d26735b0ff3b9433b
srikanth <bommisettysrikanth88@gmail.com>
2025-01-18 23:53:47 +0530

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -62,6 +62,7 @@ unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzipping the frontend code"
 
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE_NAME
+VALIDATE $? "Copied expense config"
 
 systemctl restart nginx &>>$LOG_FILE_NAME
 VALIDATE $? "Restarting ngnix"
